2c9b689dfcb40b6e3c9cc75bc840f81d6235af55
mrawat <mukeshrawat02@gmail.com>
2021-03-26 15:37:58 +0000

    Showing expression on solve button click

--- src/components/Dashboard.test.tsx
+++ src/components/Dashboard.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import { Dashboard } from './Dashboard'
+import React, { Dispatch } from "react";
 
 test('should have title', () => {
   render(<Dashboard />)
@@ -9,4 +10,36 @@ test('should have title', () => {
 test('should load FileReader component', () => {
   render(<Dashboard />)
   expect(screen.getByText(/Drop CSV file here/i)).toBeInTheDocument()
+})
+
+test('should find the target in a given array', async () => {
+  const initialState = {
+    numbers: [1, 2, 3, 4],
+    target: "12",
+    formula: "",
+  }
+
+  type State = typeof initialState;
+  const stateMock: Dispatch<Partial<State>> = jest.fn()
+
+  const stateSpy = jest.spyOn(React, 'useState') as jest.SpyInstance<
+    [State, Dispatch<State>]
+  >
+  stateSpy.mockImplementation((): [State, Dispatch<State>] => [
+    initialState,
+    stateMock,
+  ])
+  const { findByText } = render(<Dashboard />)
+  //solve should be rendered as we are passing that in initial state
+  const solve = await findByText('Solve');
+  expect(solve).toBeTruthy();
+  // fire solve click
+  fireEvent.click(solve);
+  
+  await waitFor(() => {
+    expect(stateMock).toHaveBeenLastCalledWith({
+      ...initialState,
+      ...{ formula: "4 * 3" },
+    })
+  })
 })
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import Paper from "@material-ui/core/Paper"
 import Grid from "@material-ui/core/Grid"
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles"
@@ -6,8 +6,7 @@ import { FileReader } from "./FileReader"
 import Typography from "@material-ui/core/Typography"
 import TextField from "@material-ui/core/TextField"
 import Button from "@material-ui/core/Button"
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { Box } from "@material-ui/core"
+import { solve } from "../utils/solver"
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -35,30 +34,42 @@ const useStyles = makeStyles((theme: Theme) =>
 )
 
 type ReaderContext = {
-  numbers: number[];
-  hideUploader: boolean;
-  target: string;
-  formula: string;
+  numbers: number[]
+  target: string
+  formula: string
 }
 
 export const Dashboard: React.FC = (): JSX.Element => {
-  const classes = useStyles();
-  const [loading, setLoading] = React.useState(false);
-  const [currentState, setState] = useState<ReaderContext>({
+  const classes = useStyles()
+
+  const [currentState, setState] = React.useState<ReaderContext>({
     numbers: [],
-    hideUploader: false,
     target: "",
-    formula: "",
+    formula: ""
   })
 
   const onFileReadHandler = (data: number[]) => {
     setState({ ...currentState, ...{ numbers: data } })
   }
 
-  const findSolution = () => {
-    // fine solution
-    setLoading((prevLoading) => !prevLoading);
-  };
+  const findSolution = async () => {
+    if (
+      currentState.numbers &&
+      currentState.numbers.length > 0 &&
+      currentState.target
+    ) {
+      let result = solve(
+        currentState.numbers,
+        parseInt(currentState.target)
+      )
+
+      result = result ? result : "could not find"
+      setState({
+        ...currentState,
+        ...{ formula: result.toString() },
+      })
+    }
+  }
 
   const onHandleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setState({
@@ -79,70 +90,60 @@ export const Dashboard: React.FC = (): JSX.Element => {
         justify="center"
         alignItems="center"
       >
-        {loading ? (
-          <Box className={classes.progressContainer}>
-            <CircularProgress size={80} className={classes.progress} />
-            <Typography color="textPrimary" variant="h6">
-              Please wait while the formula is creating..
-                </Typography>
-          </Box>
-        ) : (
-          <>
-            <Grid item xs={12}>
-              <Typography variant="h4" gutterBottom>
-                CSV Numbers Formula Solver
+        <Grid item xs={12}>
+          <Typography variant="h4" gutterBottom>
+            CSV Numbers Formula Solver
             </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <FileReader onFileRead={onFileReadHandler} />
+        </Grid>
+        {currentState.numbers?.length > 0 && (
+          <>
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper} elevation={3}>
+                <TextField
+                  label="Selection"
+                  defaultValue={currentState.numbers?.join(",")}
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  variant="filled"
+                />
+              </Paper>
             </Grid>
-            <Grid item xs={12}>
-              <FileReader onFileRead={onFileReadHandler} />
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper} elevation={3}>
+                <TextField
+                  id="outlined-basic"
+                  label="Target Number"
+                  variant="outlined"
+                  type="number"
+                  value={currentState.target}
+                  onChange={onHandleChange}
+                />
+              </Paper>
             </Grid>
-            {currentState.numbers?.length > 0 && (
-              <>
-                <Grid item xs={12} sm={6}>
-                  <Paper className={classes.paper} elevation={3}>
-                    <TextField
-                      label="Selection"
-                      defaultValue={currentState.numbers?.join(",")}
-                      InputProps={{
-                        readOnly: true,
-                      }}
-                      variant="filled"
-                    />
-                  </Paper>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <Paper className={classes.paper} elevation={3}>
-                    <TextField
-                      id="outlined-basic"
-                      label="Target Number"
-                      variant="outlined"
-                      type="number"
-                      value={currentState.target}
-                      onChange={onHandleChange}
-                    />
-                  </Paper>
-                </Grid>
-                {currentState.target && (
-                  <Grid item xs={12} sm={6}>
-                    <Paper className={classes.paper} elevation={3}>
-                      <Button
-                        variant="outlined"
-                        color="primary"
-                        onClick={() => queueMicrotask(() => findSolution())}
-                      >
-                        Solve
+            {currentState.target && (
+              <Grid item xs={12} sm={6}>
+                <Paper className={classes.paper} elevation={3}>
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    data-testid="Solve"
+                    onClick={() => findSolution()}
+                  >
+                    Solve
                   </Button>
-                    </Paper>
-                  </Grid>
-                )}
-                {currentState.formula && (
-                  <Grid item xs={12}>
-                    <Paper className={classes.paper} elevation={3}>
-                      {currentState.formula}
-                    </Paper>
-                  </Grid>
-                )}
-              </>
+                </Paper>
+              </Grid>
+            )}
+            {currentState.formula && (
+              <Grid item xs={12}>
+                <Paper className={classes.paper} elevation={3}>
+                  {currentState.formula}
+                </Paper>
+              </Grid>
             )}
           </>
         )}
